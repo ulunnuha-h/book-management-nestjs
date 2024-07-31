3c5c6dce7b5ce9609a482574f69728933d6f6cf9
Hanif Ulunnuha Hidayat <ulunnuha.h@gmail.com>
2024-07-31 16:08:37 +0700

    first commit

---
+++ src/books/books.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { CreateBookDto } from './dto/create-book.dto';
+import { UpdateBookDto } from './dto/update-book.dto';
+import { Book } from './entities/book.entity';
+
+@Injectable()
+export class BooksService {
+  private readonly books: Book[] = [];
+
+  create(createBookDto: CreateBookDto) {
+    return 'This action adds a new book';
+  }
+
+  findAll() {
+    return `This action returns all books`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} book`;
+  }
+
+  update(id: number, updateBookDto: UpdateBookDto) {
+    return `This action updates a #${id} book`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} book`;
+  }
+}
